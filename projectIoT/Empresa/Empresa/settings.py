676a971f2a32dfce03ad88fd95628ab1e861be58
RepellantHail <a20310068@ceti.mx>
2024-05-02 15:10:46 -0600

    sql lite added

--- projectIoT/Empresa/Empresa/settings.py
+++ projectIoT/Empresa/Empresa/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Empresa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'Empresa.db',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
