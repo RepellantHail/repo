047715d1a2d767d50f612f5c84198681ed28e9ef
RepellantHail <a20310068@ceti.mx>
2024-05-02 19:11:02 -0600

    Model updated

--- projectIoT/Empresa/Aplicaciones/Empleado/models.py
+++ projectIoT/Empresa/Aplicaciones/Empleado/models.py
@@ -2,23 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    id              =   models.AutoField(primary_key=True)
-    name            =   models.CharField(max_length=100)
-    apellido1       =   models.CharField(max_length=100)
-    apellido2       =   models.CharField(max_length=100)
-    cargo           =   models.CharField(max_length=100)
-    empresa         =   models.CharField(max_length=100)
-    calle           =   models.CharField(max_length=100)
-    numeroExt       =   models.IntegerField
-    numeroInt       =   models.CharField(max_length=10)
-    colonia         =   models.CharField(max_length=100)
-    municipio       =   models.CharField(max_length=100)
-    estado          =   models.CharField(max_length=100)
-    codPos          =   models.IntegerField
-    telefono        =   models.IntegerField
-    email           =   models.EmailField(max_length=54)
-    fechaNac        =   models.DateField(auto_now=False, auto_now_add=False)
-    edad            =   models.IntegerField
+    id              =   models.AutoField    (primary_key=True)
+    name            =   models.CharField    (max_length=100)
+    apellido1       =   models.CharField    (max_length=100)
+    apellido2       =   models.CharField    (max_length=100)
+    cargo           =   models.CharField    (max_length=100)
+    empresa         =   models.CharField    (max_length=100)
+    calle           =   models.CharField    (max_length=100)
+    numeroExt       =   models.IntegerField (default=1234)
+    numeroInt       =   models.CharField    (max_length=10)
+    colonia         =   models.CharField    (max_length=100)
+    municipio       =   models.CharField    (max_length=100)
+    estado          =   models.CharField    (max_length=100)
+    codPos          =   models.IntegerField (default=45060)
+    telefono        =   models.IntegerField (default=3331000000)
+    email           =   models.EmailField   (max_length=54)
+    fechaNac        =   models.DateField    (auto_now=False, auto_now_add=False)
+    edad            =   models.IntegerField (default=22)
 
     def __str__(self):
         texto = "{0} ({1})"
